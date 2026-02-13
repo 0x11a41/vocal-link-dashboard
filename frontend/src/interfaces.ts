287d7ac598755d5d75a6a860a31a090f11e6a385
0x11a41 <harikrishnamohan@proton.me>
2026-02-13 06:00:53 +0530

    rewound accedently removed files

---
+++ frontend/src/interfaces.ts
@@ -0,0 +1,248 @@
+export const VERSION = "v0.5-alpha";
+export const URL = "http://localhost:6210";
+
+export const ws: WebSocket = new WebSocket("ws://localhost:6210/ws/control");
+ws.onopen = ():void => ws.send(JSON.stringify(Payloads.event(WSEvents.DASHBOARD_INIT)));
+
+export interface SessionMetadata {
+  id: string;
+  name: string;
+  ip: string;
+  battery: number;
+  device: string;
+  theta: number; 
+  last_rtt: number; 
+  last_sync?: number | null;
+}
+
+export interface ServerInfo {
+  name: string;
+  ip: string;
+  active_sessions: number;
+}
+
+export enum RESTEvents {
+}
+
+export enum WSKind {
+  ACTION = "action",
+  EVENT = "event",
+  ERROR = "error",
+}
+
+export enum WSErrors {
+  INVALID_KIND = "invalid_kind",
+  INVALID_EVENT = "invalid_event",
+  INVALID_BODY = "invalid_body",
+  ACTION_NOT_ALLOWED = "action_not_allowed",
+  SESSION_NOT_FOUND = "session_not_found",
+}
+
+export enum WSEvents {
+  DASHBOARD_INIT = "dashboard_init",
+  DASHBOARD_RENAME = "dashboard_rename",
+  SESSION_UPDATE = "session_update",
+  SESSION_ACTIVATE = "session_activate",
+  SESSION_ACTIVATED = "session_activated",
+  SESSION_LEFT = "session_left",
+  SESSION_SELF_START = "session_self_start",
+  SESSION_SELF_STOP = "session_self_stop",
+  SUCCESS = "success",
+  FAIL = "failed",
+}
+
+export enum WSActions {
+  START = "start",
+  STOP = "stop",
+  STARTED = "started",
+  STOPPED = "stopped",
+}
+
+export interface Rename {
+  new_name: string;
+}
+
+export interface WSActionTarget {
+  session_id: string;
+  trigger_time?: number | null;
+}
+
+type WSBodyTypes = SessionMetadata | WSActionTarget | Rename | null;
+type WSMsgTypes = WSActions | WSEvents | WSErrors;
+
+export interface WSPayload {
+  kind: WSKind;
+  msg_type: WSMsgTypes;
+  body: WSBodyTypes;
+}
+
+export const Payloads = {
+  action: (type: WSActions, session_id: string = "all"): WSPayload => ({
+    kind: WSKind.ACTION,
+    msg_type: type,
+    body: { session_id, trigger_time: null }
+  }),
+
+  rename: (newName: string): WSPayload => ({
+    kind: WSKind.ACTION,
+    msg_type: WSEvents.DASHBOARD_RENAME,
+    body: { new_name: newName }
+  }),
+
+  event: (type: WSEvents, body: WSBodyTypes | null = null): WSPayload => ({
+    kind: WSKind.EVENT,
+    msg_type: type,
+    body: body
+  }),
+
+  error: (type: WSErrors): WSPayload => ({
+    kind: WSKind.ERROR,
+    msg_type: type,
+    body: null,
+  }),
+};
+
+
+
+function createTimerDisplayComp(): HTMLElement {
+  const timerDisplay = document.createElement('p');
+  timerDisplay.classList.add('timer');
+  timerDisplay.innerText = '00:00';
+  return timerDisplay;
+}
+
+
+export enum SessionState {
+  IDLE = "idle",
+  RECORDING = "recording"
+}
+// when user clicks start/stop button on a session, it notifies the session
+// about it. The session to send an acknowledgement back to UI, and only then
+// the visible changes are made using start() or stop() methods.
+export class Session {
+  public state: SessionState = SessionState.IDLE;
+  
+  private intervalId: number | null = null;
+  private secondsElapsed: number = 0;
+
+  public meta: SessionMetadata;
+  public card: HTMLElement;
+  public timerDisplay: HTMLElement;
+  public micBtn: HTMLElement;
+  private statusRow: HTMLElement;
+
+  constructor(meta: SessionMetadata) {
+    this.meta = meta;
+    this.timerDisplay = createTimerDisplayComp();
+    this.micBtn = roundBtn({iconName:"record-icon",onClick:() => {
+      if (this.state === SessionState.IDLE) {
+        this.notify(WSActions.START);
+      } else if (this.state === SessionState.RECORDING) {
+        this.notify(WSActions.STOP);
+      }
+    }});
+    
+    this.card = document.createElement('div');
+    this.card.classList.add("session-card");
+    
+    const left = document.createElement('div');
+    left.classList.add('left');
+    left.innerHTML = `
+        <div>
+            <b>${meta.name}</b>
+            <div class="device-name">${meta.device}</div>
+        </div>
+        `;
+
+    this.statusRow = document.createElement('div');
+    this.statusRow.classList.add('status-row');
+    this.statusRow.innerText = `🔋${meta.battery}%  📶${meta.last_rtt}ms`;
+    left.appendChild(this.statusRow);
+    
+    const right = document.createElement('div');
+    right.classList.add('right');
+    right.appendChild(this.timerDisplay);
+    right.appendChild(this.micBtn);
+
+    this.card.appendChild(left);
+    this.card.appendChild(right);
+  }
+
+  public notify(action: WSActions): void {
+    const msg = Payloads.action(action, this.meta.id);
+    console.log(msg);
+    ws.send(JSON.stringify(msg));
+  }
+
+  public start(): void {
+    this.state = SessionState.RECORDING;
+    this.micBtn.classList.remove('record-icon');
+    this.micBtn.classList.add('stop-icon');
+    this.card.classList.add('border-recording');
+    this.startTimer();
+  }
+
+  public stop(): void {
+    this.state = SessionState.IDLE;
+    this.micBtn.classList.remove('stop-icon');
+    this.micBtn.classList.add('record-icon');
+    this.card.classList.remove('border-recording');
+    this.resetTimer();
+  }
+
+  public updateMeta(newMeta: SessionMetadata): void {
+    this.meta.battery = newMeta.battery;
+    this.meta.last_rtt = newMeta.last_rtt;
+    this.meta.theta = newMeta.theta;
+    this.meta.last_sync = newMeta.last_sync;
+
+    this.statusRow.innerText = `🔋${this.meta.battery}%  📶${this.meta.last_rtt}ms`;
+  }
+
+  private startTimer(): void {
+    if (this.intervalId) return; 
+    this.intervalId = window.setInterval(() => {
+      this.secondsElapsed++;
+      this.updateTimerDisplay();
+    }, 1000);
+  }
+
+  private resetTimer(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+    this.secondsElapsed = 0;
+    this.timerDisplay.innerText = "00:00";
+  }
+
+  private updateTimerDisplay(): void {
+    const mins = Math.floor(this.secondsElapsed / 60).toString().padStart(2, '0');
+    const secs = (this.secondsElapsed % 60).toString().padStart(2, '0');
+    this.timerDisplay.innerText = `${mins}:${secs}`;
+  }
+}
+
+interface RoundButtonOptions { iconName: string; onClick: () => void; }
+function roundBtn({ iconName, onClick }: RoundButtonOptions): HTMLElement {
+  const micBtn = document.createElement('div');
+  micBtn.classList.add('btn-circle', iconName, 'highlight-on-cursor');
+  micBtn.onclick = onClick;
+  return micBtn;
+}
+
+interface ButtonOptions { label: string; classes?: string[]; onClick: () => void; }
+export function button({ label, classes = [], onClick }: ButtonOptions): HTMLButtonElement {
+  const btn = document.createElement('button');
+  btn.innerText = label;
+  btn.classList.add('highlight-on-cursor', ...classes);
+  btn.onclick = () => onClick();
+  return btn;
+}
+
+export enum Views {
+  DASHBOARD = "dashboard",
+  RECORDINGS = "recordings",
+  SETTINGS = "settings"
+}
+
